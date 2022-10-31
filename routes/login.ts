32f8acfa10826917bee21e5ad40636c97cade030
Andrew Stovell <andrewhstovell@gmail.com>
2022-10-31 21:12:08 +1030

    Completed assignment submission

--- routes/login.ts
+++ routes/login.ts
@@ -33,8 +33,17 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
-      .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+
+    if (isSQLInjection(req.body.email) || isSQLInjection(req.body.password)) {
+      res.status(400).send(res.__('SQL Injection attempt detected & logged'))
+    }
+
+    models.sequelize.query(
+      `SELECT * FROM Users WHERE 
+      email = $1 AND 
+      password = $2 
+      AND deletedAt IS NULL`, { bind: [req.body.email, security.hash(req.body.password)], model: UserModel, plain: true })
+      .then((authenticatedUser: { data: User }) => {
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
           res.status(401).json({
@@ -55,7 +64,11 @@ module.exports = function login () {
         next(error)
       })
   }
-  // vuln-code-snippet end loginAdminChallenge loginBenderChallenge loginJimChallenge
+
+  // Returns true iff the input contains any of the prohibited characters
+  function isSQLInjection (input: string) {
+    return input.includes("'") || input.includes('-') || input.includes(';')
+  }
 
   function verifyPreLoginChallenges (req: Request) {
     challengeUtils.solveIf(challenges.weakPasswordChallenge, () => { return req.body.email === 'admin@' + config.get('application.domain') && req.body.password === 'admin123' })
